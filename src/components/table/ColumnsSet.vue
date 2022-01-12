14ff85ba120dd157902bf855d8e27f4c4337f7e3
liujian <449667481@qq.com>
2022-01-12 23:24:30 +0800

    update

--- src/components/table/ColumnsSet.vue
+++ src/components/table/ColumnsSet.vue
@@ -48,29 +48,62 @@
       }
     },
     created(){
-        this.originColumns = JSON.parse(JSON.stringify(this.columns));
-        this.columnsSetting = this.columns.map(value => ({ ...value, checked: true }))
+        this.originColumns = this.deepClone(this.columns);
+        this.columnsSetting = this.columns.map(value => ({ ...value, checked: true }));
     },
     methods: {
+      // 定义一个深拷贝函数  接收目标target参数
+      deepClone: function(target) {
+        // 定义一个变量
+        let result;
+        // 如果当前需要深拷贝的是一个对象的话
+        if (typeof target === 'object') {
+        // 如果是一个数组的话
+            if (Array.isArray(target)) {
+                result = []; // 将result赋值为一个数组，并且执行遍历
+                for (let i in target) {
+                    // 递归克隆数组中的每一项
+                    result.push(this.deepClone(target[i]))
+                }
+            // 判断如果当前的值是null的话；直接赋值为null
+            } else if(target===null) {
+                result = null;
+            // 判断如果当前的值是一个RegExp对象的话，直接赋值    
+            } else if(target.constructor===RegExp){
+                result = target;
+            }else {
+            // 否则是普通对象，直接for in循环，递归赋值对象的所有值
+                result = {};
+                for (let i in target) {
+                    result[i] = this.deepClone(target[i]);
+                }
+            }
+        // 如果不是对象的话，就是基本数据类型，那么直接赋值
+        } else {
+            result = target;
+        }
+        // 返回最终结果
+        return result;
+      },
       reset() {
-        this.columnsSetting = JSON.parse(JSON.stringify(this.originColumns))
-        this.indeterminate = false
-        this.checkAll = true
-        this.emitChange()
+        this.columnsSetting = this.deepClone(this.originColumns);
+        this.indeterminate = false;
+        this.checkAll = true;
+        this.emitChange();
       },
       handleChange() {
-        let checkedList = this.columnsSetting.filter(value => value.checked)
-        this.indeterminate = !!checkedList.length && checkedList.length < this.columnsSetting.length
-        this.checkAll = checkedList.length === this.columnsSetting.length
-        this.emitChange()
+        let checkedList = this.columnsSetting.filter(value => value.checked);
+        this.indeterminate = !!checkedList.length && checkedList.length < this.columnsSetting.length;
+        this.checkAll = checkedList.length === this.columnsSetting.length;
+        this.emitChange();
       },
       handleCheckAllChange(val) {
-        this.columnsSetting = this.columns.map(value => ({ ...value, checked: val }))
+        this.columnsSetting = this.columns.map(value => ({ ...value, checked: val }));
         this.indeterminate = false;
-        this.emitChange()
+        this.emitChange();
       },
       emitChange() {
-        this.$emit('columnChange', this.columnsSetting)
+        this.$emit('columnChange', this.columnsSetting);
       }
     },
     mounted() {
